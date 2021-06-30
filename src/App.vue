95de9dc0a56a1b0b7b7f7c917841abc65957f568
Igor <info@pro-landing.site>
2021-06-30 22:16:53 +0300

    создание компонента и базовая разметка

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-
+    <PhotoProcessing/>
   </div>
 </template>
 
 <script>
-
+import PhotoProcessing from './components/PhotoProcessing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PhotoProcessing
   }
 }
 </script>
 
 <style lang="scss">
-
+body{
+  font-family: Arial, Helvetica, sans-serif;
+}
 </style>
---
+++ src/components/PhotoProcessing.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main-wrapper">
+     <img src="@/assets/img/vue.jpeg" alt="logo-vue" width="500">
+     <div class="photo-parameters">
+        <h1>Онлайн редактор:</h1>
+
+         <h4>Видимость изображения:</h4>
+         <button type="button">Показать / Скрыть</button>
+
+         <h4>Фильтры</h4>
+         <button type="button">Сепия</button>
+         <button type="button">Рамка</button>
+         <button type="button">Тень</button>
+         <button type="button">Обводка</button>
+         <button type="button">Размытие</button>
+         <button type="button">Яркость</button>
+         <button type="button">Яркость</button>
+         <button type="button">Контраст</button>
+         <button type="button">Оттенок серого</button>
+         <button type="button">Оттенок-поворот</button>
+         <button type="button">Инвертировать</button>
+         <button type="button">Постеризовать</button>
+
+         <h4>Прозрачность:</h4>
+         <input type="range">
+
+         <h4>Размер:</h4>
+         <input type="range">
+
+         <h4>Поворот:</h4>
+         <input type="range">
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
