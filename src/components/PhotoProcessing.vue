02aca91413f0841faf9f3dc0fd2276db068232be
Igor <info@pro-landing.site>
2021-06-30 22:54:53 +0300

    добавил базовый стиль

--- src/components/PhotoProcessing.vue
+++ src/components/PhotoProcessing.vue
@@ -1,44 +1,109 @@
 <template>
-  <div class="main-wrapper">
-     <img src="@/assets/img/vue.jpeg" alt="logo-vue" width="500">
-     <div class="photo-parameters">
-        <h1>Онлайн редактор:</h1>
+  <div>
+    <div class="main-wrapper">
+      <img src="@/assets/img/vue.jpeg" alt="logo-vue" width="500" />
+      <div class="photo-parameters">
+        <div class="visibility-wrapper">
+          <h4>Видимость изображения:</h4>
+          <button class="show-button" type="button">Показать / Скрыть</button>
+        </div>
 
-         <h4>Видимость изображения:</h4>
-         <button type="button">Показать / Скрыть</button>
+        <h4>Фильтры:</h4>
+        <div class="button-wrapper">
+          <button class="photo-parameters__button" type="button">Сепия</button>
+          <button class="photo-parameters__button" type="button">Рамка</button>
+          <button class="photo-parameters__button" type="button">Тень</button>
+          <button class="photo-parameters__button" type="button">
+            Обводка
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Размытие
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Яркость
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Яркость
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Контраст
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Оттенок серого
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Оттенок-поворот
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Инвертировать
+          </button>
+          <button class="photo-parameters__button" type="button">
+            Постеризовать
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="input-wrapper">
+      <h4>Прозрачность:</h4>
+      <input type="range" />
 
-         <h4>Фильтры</h4>
-         <button type="button">Сепия</button>
-         <button type="button">Рамка</button>
-         <button type="button">Тень</button>
-         <button type="button">Обводка</button>
-         <button type="button">Размытие</button>
-         <button type="button">Яркость</button>
-         <button type="button">Яркость</button>
-         <button type="button">Контраст</button>
-         <button type="button">Оттенок серого</button>
-         <button type="button">Оттенок-поворот</button>
-         <button type="button">Инвертировать</button>
-         <button type="button">Постеризовать</button>
+      <h4>Размер:</h4>
+      <input type="range" />
 
-         <h4>Прозрачность:</h4>
-         <input type="range">
-
-         <h4>Размер:</h4>
-         <input type="range">
-
-         <h4>Поворот:</h4>
-         <input type="range">
-     </div>
+      <h4>Поворот:</h4>
+      <input type="range" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.main-wrapper {
+  display: flex;
+  align-items: center;
+}
+.photo-parameters {
+  margin-left: 25px;
+  &__button {
+    width: 48%;
+    cursor: pointer;
+    padding: 5px 10px;
+    border: 1px solid black;
+    transition: 0.2s ease-in;
+    margin: 0 5px 5px 0;
+  }
+  &__button:hover {
+    background-color: rgb(250, 173, 73);
+  }
+}
+.button-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 400px;
+}
+h4 {
+  margin: 10px 0;
+}
+input[type="range"] {
+  width: 500px;
+}
+.input-wrapper {
+  margin-top: 25px;
+}
+.visibility-wrapper{
+   margin-bottom: 25px;
+}
+.show-button{
+   cursor: pointer;
+   padding: 5px 10px;
+   border: 1px solid black;
+   transition: 0.2s ease-in;
+}
+.show-button:hover{
+   background-color: rgb(250, 173, 73);
+}
 </style>
